9156b200bc312b796ada48c33572d16e8e5d3199
Kimani Kabiria <kabiriakimani@gmail.com>
2023-07-11 23:23:27 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-Module 1-number_of_lines
-
-Contains function that returns number of lines in text file
-"""
-
 
 def number_of_lines(filename=""):
     """Return number of lines in text file"""
