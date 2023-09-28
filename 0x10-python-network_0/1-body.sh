1c53cced530f5a18fb4e9ca0c4ca02c064e27885
Kimani Kabiria <kabiriakimani@gmail.com>
2023-09-29 00:35:10 +0300

    0x10-python-network_0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Display only body of a 200 status code response
-VAR=$(curl -so /dev/null -I -w "%{http_code}" "$1");
-
-if [ "$VAR" == 200 ]; then
-    curl -sL "$1"
-fi
+# Sends a GET request to the URL, and displays the body of the response
+curl -s -L "${1}"
